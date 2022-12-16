efc46720513810c7e5b571557442dbcb7c8cddb5
Ravindra Devrani <ravindra.devrani003@gmail.com>
2022-12-16 17:12:51 +0530

    last push

--- CsharpTutorial/part32.cs
+++ CsharpTutorial/part32.cs
@@ -8,9 +8,41 @@ namespace CsharpTutorial
 {
     internal class part32
     {
-        static void Main()
+        public static int PrintA()
         {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"A{i}");
+            }
+            Thread.Sleep(5000);
+            return 1;
+        }
 
+
+        public static async Task<int> PrintB()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"A{i}");
+            }
+            await Task.Delay(5000);
+            return 1;
+        }
+
+
+
+        static async Task Main(string[] args)
+        {
+            //int taskA = PrintA();
+            //Console.WriteLine("i am outside from task A");
+            //Console.WriteLine("i am also outside from task A");
+            //Console.WriteLine($"Result of task A is {taskA}");
+
+            Task<int> taskB = PrintB();
+            Console.WriteLine("i am outside from task B");
+            Console.WriteLine("i am also outside from task B");
+            await taskB;
+            Console.WriteLine($"Result of task A is {taskB.Result}");
         }
     }
 }
